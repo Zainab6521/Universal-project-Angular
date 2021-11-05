20d9390ce8d3b80d261d1f79760142f61066e01b
zainab raza <zk.652162@gmail.com>
2021-11-05 12:24:13 +0500

    UNIVERSAL PROJECT: SSR with Jquery. Solved: '$' is not defined.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import * as $ from 'jquery';
+// window['$'] = window['jquery'] = $;
+// declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(@Inject(PLATFORM_ID) private platformId: any) {}
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      $('button').click(function () {
+        $('.div1').remove();
+      });
+    }
+  }
   title = 'ssr';
 }
